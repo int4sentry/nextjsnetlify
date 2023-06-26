9bc1a62644073b406b49bcec92ae8debf959aee8
kerenkhatiwada <keren.khatiwada@sentry.io>
2023-06-26 13:39:23 -0700

    “net”

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   // Your existing module.exports
+  productionBrowserSourceMaps: true,
 
   sentry: {
     // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
@@ -15,6 +16,10 @@ const moduleExports = {
     // https://webpack.js.org/configuration/devtool/ and
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
     // for more information.
+    disableClientWebpackPlugin: true,
+    disableServerWebpackPlugin: true,
+    widenClientFileUpload: true,
+
     hideSourceMaps: true,
   },
 };
